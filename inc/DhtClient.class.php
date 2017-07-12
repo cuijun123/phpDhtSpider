3195d4d3f914bd72ae8e20d13f846649b146b47d
cuijun <7146275@qq.com>
2017-07-12 16:30:42 +0800

    fixbug

--- inc/DhtClient.class.php
+++ inc/DhtClient.class.php
@@ -41,20 +41,20 @@ class DhtClient
     {
         switch ($msg['q']) {
             case 'ping'://确认你是否在线
-                //echo '朋友'.$address[0].'正在确认你是否在线'.PHP_EOL;
+                echo '朋友'.$address[0].'正在确认你是否在线'.PHP_EOL;
                 self::on_ping($msg, $address);
                 break;
             case 'find_node': //向服务器发出寻找节点的请求
-                //echo '朋友'.$address[0].'向你发出寻找节点的请求'.PHP_EOL;
+                echo '朋友'.$address[0].'向你发出寻找节点的请求'.PHP_EOL;
                 self::on_find_node($msg, $address);
                 break;
             case 'get_peers':
-                //echo '朋友'.$address[0].'向你发出查找资源的请求'.PHP_EOL;
+                echo '朋友'.$address[0].'向你发出查找资源的请求'.PHP_EOL;
                 // 处理get_peers请求
                 self::on_get_peers($msg, $address);
                 break;
             case 'announce_peer':
-                //echo '朋友' . $address[0] . '找到资源了 通知你一声' . PHP_EOL;
+                echo '朋友' . $address[0] . '找到资源了 通知你一声' . PHP_EOL;
                 // 处理announce_peer请求
                 self::on_announce_peer($msg, $address);
                 break;
