7f0330b523532f6ec2cced07b3d0cded3819cfa8
灯火阑珊 <7146275@qq.com>
2017-07-14 15:41:16 +0800

    fixbug

--- inc/DhtClient.class.php
+++ inc/DhtClient.class.php
@@ -41,20 +41,20 @@ class DhtClient
     {
         switch ($msg['q']) {
             case 'ping'://确认你是否在线
-                echo '朋友'.$address[0].'正在确认你是否在线'.PHP_EOL;
+                //echo '朋友'.$address[0].'正在确认你是否在线'.PHP_EOL;
                 self::on_ping($msg, $address);
                 break;
             case 'find_node': //向服务器发出寻找节点的请求
-                echo '朋友'.$address[0].'向你发出寻找节点的请求'.PHP_EOL;
+                //echo '朋友'.$address[0].'向你发出寻找节点的请求'.PHP_EOL;
                 self::on_find_node($msg, $address);
                 break;
             case 'get_peers':
-                echo '朋友'.$address[0].'向你发出查找资源的请求'.PHP_EOL;
+                //echo '朋友'.$address[0].'向你发出查找资源的请求'.PHP_EOL;
                 // 处理get_peers请求
                 self::on_get_peers($msg, $address);
                 break;
             case 'announce_peer':
-                echo '朋友' . $address[0] . '找到资源了 通知你一声' . PHP_EOL;
+                //echo '朋友' . $address[0] . '找到资源了 通知你一声' . PHP_EOL;
                 // 处理announce_peer请求
                 self::on_announce_peer($msg, $address);
                 break;
@@ -156,10 +156,11 @@ class DhtClient
             'y' => 'r',
             'r' => array(
                 'id' => Base::get_neighbor($id, $nid),
-                'nodes' => Base::encode_nodes(self::get_nodes()),
+                'nodes' => "",
                 'token' => substr($infohash, 0, 2)
             )
         );
+
 
         // 将node加入路由表
         self::append(new Node($id, $address[0], $address[1]));
@@ -175,7 +176,7 @@ class DhtClient
      */
     public static function on_announce_peer($msg, $address)
     {
-        global $nid, $queue;
+        global $nid,$serv;
         $infohash = $msg['a']['info_hash'];
         $port = $msg['a']['port'];
         $token = $msg['a']['token'];
@@ -195,10 +196,6 @@ class DhtClient
         if ($port >= 65536 || $port <= 0) {
             return;
         }
-        
-		if($port == 6881){ //6881端口基本被封
-			return;
-		}
 
         if ($tid == '') {
             //return;
@@ -216,108 +213,67 @@ class DhtClient
         // 发送请求回复
         DhtServer::send_response($msg, $address);
         $ip = $address[0];
-        $queue->push(array($ip,$port,$infohash));
-        //Func::Logs('magnet:?xt=urn:btih:' . strtoupper(bin2hex($infohash)).PHP_EOL,2);
-        //echo $ip.':'.$port.PHP_EOL;
+        swoole_process::wait(false);
+        $process = new swoole_process(function (swoole_process $worker) use($ip,$port,$infohash){
+            $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
+            if (!@$client->connect($ip, $port, 2))
+            {
+                // echo ("connect failed. Error: {$client->errCode}".PHP_EOL);
+            }else{
+                echo 'connent success! '.$ip.':'.$port.PHP_EOL;
+                $rs = Metadata::download_metadata($client,$infohash);
+                if($rs != false){
+                    echo  $rs['name'].PHP_EOL;
+                    $data = Db::get_one("select 1 from history where infohash = '$rs[infohash]' limit 1");
+                    if(!$data){
+                        Db::insert('history',array('infohash'=>$rs['infohash']));
+                        $files = '';
+                        $length = 0;
+                        if($rs['files'] !=''){
+                            $files = json_encode($rs['files']);
+                            foreach ($rs['files'] as $value){
+                                $length += $value['length'];
+                            }
+                        }else{
+                            $length = $rs['length'];
+                        }
+                        Db::insert('bt',array(
+                                'name'=>$rs['name'],
+                                'keywords'=>Func::getKeyWords($rs['name']),
+                                'infohash'=>$rs['infohash'],
+                                'files'=>$files,
+                                'length'=>$length,
+                                'piece_length'=>$rs['piece_length'],
+                                'hits'=>0,
+                                'time'=>date('Y-m-d H:i:s'),
+                                'lasttime'=>date('Y-m-d H:i:s'),
+                            )
+                        );
+                    }else{
+                        Db::query("update bt set `hot` = `hot` + 1 where infohash = '$rs[infohash]'");
+                    }
+                }
+                $client->close(true);
+            }
+            $worker->exit(0);
+        }, false);
+        $process->start();
     }
 
-    public static function get_nodes($len = 8)
-    {
-        global $table;
+public static function get_nodes($len = 8)
+{
+    global $table;
 
-        if (count($table) <= $len)
-            return $table;
+    if (count($table) <= $len)
+        return $table;
 
-        $nodes = array();
+    $nodes = array();
 
-        for ($i = 0; $i < $len; $i++) {
-            $nodes[] = $table[mt_rand(0, count($table) - 1)];
-        }
-
-        return $nodes;
+    for ($i = 0; $i < $len; $i++) {
+        $nodes[] = $table[mt_rand(0, count($table) - 1)];
     }
 
-    public static function send_handshake($cli, $infohash)
-    {
-        $bt_protocol = self::$_bt_protocol;
-        $bt_header = chr(strlen($bt_protocol)) . $bt_protocol;
-        $ext_bytes = "\x00\x00\x00\x00\x00\x10\x00\x00";
-        $peer_id = Base::get_node_id();
-        $packet = $bt_header . $ext_bytes . $infohash . $peer_id;
-        if ($cli->send($packet) == false) {
-            $cli->close();
-        }
-        return;
-    }
+    return $nodes;
+}
 
-    public static function check_handshake($cli, $packet, $self_infohash)
-    {
-        $bt_header_len = ord(substr($packet, 0, 1));
-        $packet = substr($packet, 1);
-        if ($bt_header_len != strlen(self::$_bt_protocol)) {
-            return false;
-        }
-
-        $bt_header = substr($packet, 0, $bt_header_len);
-        $packet = substr($packet, $bt_header_len);
-        if ($bt_header != self::$_bt_protocol) {
-            return false;
-        }
-
-        $packet = substr($packet, 8);
-        $infohash = substr($packet, 0, 20);
-
-        if ($infohash != $self_infohash) {
-            return false;
-        }
-        return true;
-    }
-
-
-    public static function send_ext_handshake($cli)
-    {
-        $msg = chr(self::$BT_MSG_ID) . chr(self::$EXT_HANDSHAKE_ID) . Base::encode(array("m" => array("ut_metadata" => 1)));//{"m":{"ut_metadata": 1}
-        $msg_len = pack("I", strlen($msg));
-        if (!BIG_ENDIAN) {
-            $msg_len = strrev($msg_len);
-        }
-        $packet = $msg_len . $msg;
-        if ($cli->send($packet) == false) {
-            $cli->close();
-        }
-        return;
-    }
-
-    public static function get_ut_metadata($data)
-    {
-        $ut_metadata = '_metadata';
-        $index = strpos($data, $ut_metadata) + strlen($ut_metadata) + 1;
-        return intval($data[$index]);
-    }
-
-
-    public static function get_metadata_size($data)
-    {
-        $metadata_size = 'metadata_size';
-        $start = strpos($data, $metadata_size) + strlen($metadata_size) + 1;
-        $data = substr($data, $start);
-        $e_index = strpos($data, "e");
-        return intval(substr($data, 0, $e_index));
-    }
-
-    //bep_0009
-    public static function request_metadata($cli, $ut_metadata, $piece)
-    {
-        $msg = chr(self::$BT_MSG_ID) . chr($ut_metadata) . Base::encode(array("msg_type" => 0, "piece" => $piece));
-        $msg_len = pack("I", strlen($msg));
-        if (!BIG_ENDIAN) {
-            $msg_len = strrev($msg_len);
-        }
-        $_msg = $msg_len . $msg;
-
-        if ($cli->send($_msg) == false) {
-            $cli->close();
-        }
-        return;
-    }
 }
