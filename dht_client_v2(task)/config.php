18578eff94e72d5658bed613925963d8e9bd40e2
E3\Administrator <7146275@qq.com>
2017-10-23 14:47:43 +0800

    优化代码 降低cpu占用

--- dht_client_v2(task)/config.php
+++ dht_client_v2(task)/config.php
@@ -3,6 +3,6 @@ return array(
     'daemonize'=>false,//是否后台守护进程
     'worker_num'=>1,// 主进程数, 一般为CPU的1至4倍 同时执行任务数量
 	'task_worker_num'=>200,//task进程的数量 值越大 CPU占用越高
-    'server_ip'=>'35.185.171.254',//服务端ip
+    'server_ip'=>'35.35.35.35',//服务端ip
     'server_port'=>2345,//服务端端口
 );
